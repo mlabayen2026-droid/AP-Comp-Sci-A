18d3029957d5e76c1f6825f4d190562e3192e983
mlabayen2026-droid <mlabayen2026@cchsdons.com>
2025-10-14 09:26:31 -0700

    Update AnatomyOfAClass.java

--- AnatomyOfAClass.java
+++ AnatomyOfAClass.java
@@ -4,71 +4,16 @@
 
 package com.mycompany.anatomyofaclass;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author MLabayen2026
  */
 public class AnatomyOfAClass {
 
-    public static void main(String[] args) {
-
-//Cars!
-//MakeModel, HP, TopMPH, Cost
-Car Cmax = new Car("Ford C-max", 188, 115, 25000); 
-System.out.println(Cmax);
-        }
-}
-
-class Car {
-    private String MakeModel;
-    private int hp;
-    private double TopMPH;
-    private double Cost;
-    
-    //set up constructor
-  public Car(String MakeModel, int hp, double TopMPH, double Cost) {
-        this.MakeModel = MakeModel;
-        this.hp = hp;
-        this.TopMPH = TopMPH;
-        this.Cost = Cost;
-
-
-
-
-
-
-
-
-
-
-/*
-class Car{
-private String carBrand = "";
-private int horsePower = 0;
-
-public Car(String inBrand){
-carBrand = inBrand;
-}
-
-Car my2ndCar = new Car("BruceCar");
-
-
-public Car(String inBrand, int inHp){
-carBrand = inBrand;
-}
-public void setHp(int hp){
-    horsePower = hp;
-}
-public int getHp(){
-    return horsePower;
-}
-public String toString(){
-String myString = "The Car Brand Is: " + carBrand;
-System.out.println(my2ndCar);
-return myString;
-
-*/
-        /*Write a class called Car that has 1 string variable called carBrand
+      /*Write a class called Car that has 1 string variable called carBrand
 An int variable called enginePower (in Horse Power)
 A double variable called maxSpeed (in MPH)
 A double variable called price (in $)
@@ -88,7 +33,94 @@ Write a driver method in main to create car types VW Bug,
 Toyota Prius, BMW i8, Tesla Model 3, 
 give them the enginePower and maxSpeed as you wish, 
 and print these details out.*/
-        
+    
+    public static void main(String[] args) {
+
+//Cars!
+//MakeModel, HP, TopMPH, Cost
+Car Bug = new Car("vwBug", 188, 115, 25000); 
+Car Prius = new Car("Toyota Prius", 191, 110, 28000);
+Car i8 = new Car("Bmw i8", 450, 155, 48000);
+Car Model3 = new Car("Tesla Model 3", 168, 115, 42000);
+
+//sepcific constructor
+
+Car Rivian = new Car("Rivian");
+Rivian.setTopMPH(135);
+Rivian.setCost(80000);
+Rivian.setMakeModel("Rivian");
+Rivian.sethp(200);
+
+
+System.out.println(Bug);
+System.out.println(Prius);
+System.out.println(i8);
+System.out.println(Model3);
+System.out.println(Rivian);
+        }
+}
+
+class Car {
+    private String MakeModel;
+    private int hp;
+    private double TopMPH;
+    private double Cost;
+    
+    //set up constructor
+  public Car(String inModel, int inhp, double inMPH, double inCost) {
+      MakeModel = inModel;
+      hp = inhp;
+      TopMPH = inMPH;
+      Cost = inCost;
+              
+  }
+  //for rivian
+  public Car(String inModel) {
+      MakeModel = inModel;
+  }
+
+    //The returns
+    //BRAND
+
+    public String getMakeModel() {
+        return MakeModel;
+    }
+    public void setMakeModel (String inModel){
+        MakeModel = inModel;
+    }
+
+
+    //ENGINE
+    public int gethp() {
+        return hp;
+    }
+    public void sethp(int inhp) {
+        hp = inhp;
+    }
+
+    //SPEED
+    public double getTopMPH() {
+        return TopMPH;
+    }
+    public void setTopMPH(double inMPH) {
+         TopMPH = inMPH;
+    }
+
+    //PRICE
+    public double getCost() {
+        return Cost;
+    }
+    public void setCost(double inCost) {
+        Cost = inCost;
+    }
+    
+     //toString method to print all car details 
+    @Override
+    public String toString(){
+        return "Make and Model: " + MakeModel + "\nHorsepower: " + hp + " HP" + "\nMax Speed : " + TopMPH + " MPH" + "\nCost: $" + Cost + "\n";
         
     }
+
 }
+
+

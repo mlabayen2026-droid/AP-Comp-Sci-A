bfcbd8b90325446b7c8dda977e9e1e08188e9fd0
mlabayen2026-droid <mlabayen2026@cchsdons.com>
2026-05-01 14:22:49 -0700

    Add files via upload

--- Escondido.java
+++ Escondido.java
@@ -25,8 +25,12 @@ public class Escondido {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //creates the game files needed to play
+        GameFiles();
+        
+        
         // TODO code application logic here
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         boolean running = true;
         
@@ -35,16 +39,16 @@ public class Escondido {
                 + "\nPress F to Start"
                 + "\nPress R for Rules"
                 + "\nPress E to Exit");
-        String MenuButton = scanner.nextLine();
+        String MenuButton = input.nextLine();
         if (MenuButton.equalsIgnoreCase("F")){
-            startGame();
+            startGame(input);
         }
         else if (MenuButton.equalsIgnoreCase("R")){
             System.out.println("\nRules:"
-                    + "\nYou wakeup at the Transit Center everyday"
-                    + "\nYou Fall asleep at 6pm everyday"
+                    + "\nYou wake up at the Transit Center everyday"
+                    + "\nYou fall asleep at 6pm everyday"
                     + "\nYou can hold 3 items at a time"
-                    + "\nYou can Vist 3 locations everyday"
+                    + "\nYou can vist 3 locations everyday"
                     + "\nYou can interact 5 times per location");
         }
         else if (MenuButton.equalsIgnoreCase("E")){
@@ -55,17 +59,83 @@ public class Escondido {
         }
     }
        
+  
       
 }
-      public static void startGame(){
-          Scanner scanner = new Scanner(System.in);
+    
+    public static void GameFiles(){
+        
+        File GameFiles = new File("Game Files");
+        if (!GameFiles.exists()){
+            GameFiles.mkdir(); //this creates the Folder
+        }
+        
+        //now create the 3 game files
+        String[] fileNames = {"inventory.txt", "box.txt", "save.txt"};
+                for (String name: fileNames) {
+                    
+                    //puts the new files in the folder
+                    File file = new File(GameFiles, name); 
+                    try{
+                        if (file.createNewFile()) {
+                        }
+                    } catch (IOException e){
+                            
+                            }
+                    
+                }
+              
+    }
+    
+    
+    
+      public static void startGame(Scanner input){
+         
           
             System.out.println("\nStarting Game...");
-            System.out.println("You wake up in the Escondido Transit Center"
+            
+            boolean playing = true;
+            
+            while (playing){
+            System.out.println("\nYou wake up in the Escondido Transit Center"
                     + "\nThere's a box on the ground"
                     + "\nWhat would you like to do?"
+                    + "\n"
                     + "\nInteract with the box [e]"
-                    + "\nStart your day [f]");
-            String Input = scanner.nextLine(); 
+                    + "\nStart your day [f]"
+                    + "\nReturn to main Menu [r]");
+            String Input = input.nextLine(); 
+            
+            if (Input.equalsIgnoreCase("e")){
+                //box logic
+            }
+            
+            else if (Input.equalsIgnoreCase("f")){
+                //game logic
+                InGameDay(input);
+            }
+            
+            else if (Input.equalsIgnoreCase("r")){
+                playing = false;
+            }
+            
+            else{
+                System.out.println("That was not an option");
+            }
+                
+            
+            }
         }
+      
+      public static void InGameDay (Scanner Input){
+          
+          int elapsedTime = 0;
+          while (elapsedTime < 360){
+          System.out.println("Interact +20minutes");
+          }
+                  
+      }
+      
+      
+      
 }
